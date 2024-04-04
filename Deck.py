780deee04e5d098ffd6c1f056363c1f1fdc99450
bighsueh <ss313021490@gmail.com>
2024-04-05 00:54:25 +0800

    edit(Deck) : 1. 在initDeck()後shuffle() 2. 調整載入的方式，必須一項一項載入

--- Deck.py
+++ Deck.py
@@ -1,21 +1,22 @@
-from Card import Card, Rank, Suit
+from Card import Card
+from Card import Rank
+from Card import Suit
 from typing import List
 
 import random
 
 class Deck():
     def __init__(self) -> None:
-        self.cards = List[Card]
+        self.cards: List[Card] = []
     
     def shuffle(self) -> None:
-        if not len(self.cards) > 0 : return None
         random.shuffle(self.cards)
     
     def initDeck(self) -> None:
         for suit in Suit:
             for rank in Rank:
                 self.cards.append(Card( suit = suit, rank = rank))
-        pass
+        self.shuffle()
     
     def drawCard(self) -> Card:
         return self.cards.pop() 
