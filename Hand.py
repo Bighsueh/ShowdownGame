728daa67d6c49861400240ac734de81e8e74d60c
bighsueh <ss313021490@gmail.com>
2024-04-05 17:19:49 +0800

    remove(hand) : 1. 由於 showCard 方法會因player type有所不同，因此將這部分的程式碼移到player

--- Hand.py
+++ Hand.py
@@ -9,22 +9,10 @@ class Hand():
     def countCards(self) -> int:
         return len(self.cards)
     
-    def showCard(self) -> Card:
-        print('當前手牌如下: ')
-        for index, card in enumerate(self.cards):
-            print(f'{index} => {card.suit}{card.rank}')
-        
-        cardIndex = int(input('請輸入欲出牌的編號: '))
-        
-        card = self.cards[cardIndex]
-        self.cards.remove(card)
-        
-        return card
-
     def addCard(self, card: Card) -> None:
         self.cards.append(card)
 
-    def setTurnCounter(self, int) -> None:
+    def setTurnCounter(self) -> None:
         self.turnCounter = 3
         
     def resetTurnCounter(self) -> None:
